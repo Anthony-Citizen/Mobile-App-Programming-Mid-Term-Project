ed9a497fe24cd6b9f3eebade91c69b3b3ae77672
Anthony Rafael <64669631+Anthony-Citizen@users.noreply.github.com>
2021-10-08 21:36:55 +0700

    Second Commit
    Add Reference

--- app/src/main/java/com/example/anthonyrafael_00000038087_if570_al_uts/Profile.java
+++ app/src/main/java/com/example/anthonyrafael_00000038087_if570_al_uts/Profile.java
@@ -7,7 +7,7 @@ import android.text.method.LinkMovementMethod;
 import android.widget.TextView;
 
 public class Profile extends AppCompatActivity {
-    TextView textView;
+    TextView textView,tvRef1,tvRef2,tvRef3,tvRef4,tvRef5,tvRef6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +16,24 @@ public class Profile extends AppCompatActivity {
 
         textView = findViewById(R.id.tvName);
         textView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        tvRef1 = findViewById(R.id.tvRef1);
+        tvRef1.setMovementMethod(LinkMovementMethod.getInstance());
+
+        tvRef2 = findViewById(R.id.tvRef2);
+        tvRef2.setMovementMethod(LinkMovementMethod.getInstance());
+
+        tvRef3 = findViewById(R.id.tvRef3);
+        tvRef3.setMovementMethod(LinkMovementMethod.getInstance());
+
+        tvRef4 = findViewById(R.id.tvRef4);
+        tvRef4.setMovementMethod(LinkMovementMethod.getInstance());
+
+        tvRef5 = findViewById(R.id.tvRef5);
+        tvRef5.setMovementMethod(LinkMovementMethod.getInstance());
+
+        tvRef6 = findViewById(R.id.tvRef6);
+        tvRef6.setMovementMethod(LinkMovementMethod.getInstance());
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
